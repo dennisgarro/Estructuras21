81f583848b476f08dc351551d53ece8debcf130b
Docente ITM <docenteitm@salasfr.itm.edu.co>
2026-02-09 09:19:54 -0500

    creacion de suma de dos numeros

--- nacholee.java
+++ nacholee.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class nacholee {
     public static void main(String[] args) {
-        System.out.println("Hola mundo ");
+        System.out.println("vamos a sumar dos numeros  ");
+        int n1 = 0, n2 = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el numero 1");
+        n1= sc.nextInt();
+        System.out.println("Ingrese el  numero 2 ");
+        n2= sc.nextInt();
+        System.out.println("La suma de los numeros es: " + (n1+n2));
+
     }
 }
