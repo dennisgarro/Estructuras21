5a0fdaec6550f015ec5a26ce1434e23e2f3cbac9
Docente ITM <docenteitm@salasfr.itm.edu.co>
2026-02-09 09:27:51 -0500

    creacion de nuevo metodo

--- nacholee.java
+++ nacholee.java
@@ -6,10 +6,14 @@ public class nacholee {
         int n1 = 0, n2 = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el numero 1");
-        n1= sc.nextInt();
+        n1 = sc.nextInt();
         System.out.println("Ingrese el  numero 2 ");
-        n2= sc.nextInt();
-        System.out.println("La suma de los numeros es: " + (n1+n2));
+        n2 = sc.nextInt();
+        System.out.println(SumaNUmeros(n1, n2));
 
+    }
+
+    public static int SumaNUmeros(int n1, int n2) {
+        return n1 + n2;
     }
 }
